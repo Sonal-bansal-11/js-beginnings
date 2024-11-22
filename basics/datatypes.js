abdc9bdd06fe95afc4306b0e01151fa6b8fba057
Sonal Bansal <157891272+Sonal-bansal-11@users.noreply.github.com>
2024-11-22 11:57:06 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,40 @@
+"use strict"; //treat all js code as newer version of javascript(classes,module,arrowfunction)
+
+//alert("hello")//we  are using nodejs not browser //alert is not defined error //read alert mdn documentation
+//alert(3+3) // in html  file or google console this code is fine in this alert has diff syntax
+
+//console.log(3+3) console.log("sonal"); //unexpected identifier 'console'
+
+//code should be readable (code readability high)and future proof
+
+//js documentation standards original- tc39.es ecmascript
+
+console.log(3+3); console.log("sonal");
+
+let name="sonal" //string
+let age=20  //number
+let isLoggedIn=false //boolean
+let State; //undefined
+let temp=null //null
+
+//primitive datatypes
+//number=>2 to power 53 range
+//bigInt
+//string(''or"")
+//boolean=.true or false
+//null (standalone value) representation of empty value
+//undefined (variable declared values not assigned)
+//symbol(used for uniqueness in figma react)
+
+//objects
+
+console.log(typeof "sonal");//string
+console.log(typeof age);//number
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
+
+
+
+
+
---
+++ basics/variables.js
@@ -0,0 +1,23 @@
+const accountId=144553 //cannot be changed
+let accountEmail="sonalbansal845@gmail.com" 
+var accountPassword="12345"
+accountCity="Prayagraj"
+let accountState;//undefined output
+
+//semicolon is optional
+
+//accountId=2 //not allowed
+
+/*
+Prefer not to use var 
+bcoz of issue of block scope and functional scope
+*/
+
+accountEmail="ruchiagarwal9085@gmail.com"
+accountPassword="67890"
+accountCity="Lucknow"
+
+console.log(accountId);//for displaying output
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
